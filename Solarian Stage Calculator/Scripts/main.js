c151c9349360294688f94fd2f666b7e85b20ccae
ACodingSeal <119761723+ACodingSeal@users.noreply.github.com>
2023-01-14 10:59:19 +1100

    Update main.js

--- Solarian Stage Calculator/Scripts/main.js
+++ Solarian Stage Calculator/Scripts/main.js
@@ -1,7 +1,7 @@
 function addSolarianStageCalculator() {
 	if (document.getElementById("SolarianStageCalculator") !== null && document.getElementById("SolarianStageCalculatorContainer") === null) {
 	console.log("[Solarian Stage Calculator] [LOG] ID located. Running script.");
-	document.getElementById("SolarianStageCalculator").innerHTML = "<div id='SolarianStageCalculatorContainer' class='templatedesktop' style='color:#FFF;padding:1em;background:#2F0075;width:80%;margin:auto'><div style='text-align:center;font-size:24px'><img src='https://acodingseal.github.io/Solarian Stage Calculator/Assets/Sword.png' width='100'/> Solarian Stage Calculator <img src='https://acodingseal.github.io/Solarian Stage Calculator/Assets/Shield.png' width='100'/><br><div style='font-size:initial'><p>Stage: <input id='CurrentStageInput' style='width:5%'/></p><p>Solarian Challenge: <input id='ChallengeInput' class='slider' type='range' value='0' min='0' max='2'/></p><p style='font-size:14px'>Selected challenge: <span id='SelectedChallengeText'/></p></div></div><table style='margin:auto;color:#FFF'><tr><td><b>Your offence:</b> <input id='PlayerOffenceInput' style='width:10%'/></td><td style='text-align:right'><b>Enemy offence:</b> <span id='EnemyOffenceOutput'/></td></tr><tr><td><b>Your defence:</b> <input id='PlayerDefenceInput' style='width:10%'/></td><td style='text-align:right'><b>Enemy defence:</b> <span id='EnemyDefenceOutput'/></td></tr><tr><td><b>Your maximum health (100x defence):</b> <span id='PlayerMaximumHealthOutput'/></td><td style='text-align:right'><b>Enemy maximum health (100x defence):</b> <span id='EnemyMaximumHealthOutput'/></td></tr><tr><td><b>Your current health (optional, default 100% of max health):</b> <input id='PlayerCurrentHealthInput' style='width:10%'/></td><td style='text-align:right'><b>Enemy current health (optional, default 100% of max health):</b> <input id='EnemyCurrentHealthInput' style='width:10%'/></td></tr></table><div style='margin-top:1em;text-align:center'><button id='SSCCalculateButton'>Calculate</button></div><br><div class='templatedesktop' style='background:initial;border-left:0;border-right:0;border-bottom:0;padding:0.5em;border-radius:initial;text-align:center'><div style='font-size:20px;font-weight:bold'>Result</div><br>At Stage <span id='CurrentStageOutput'>?</span>, the requirements to complete the stage, when currently <span id='CurrentHealthOutput'>?</span>% of maximum health, are:<br><br><img src='https://acodingseal.github.io/Solarian Stage Calculator/Assets/Sword.png' width='50'/> Offence: <span id='PlayerOffenceReqOutput'>?</span><br><img src='https://acodingseal.github.io/Solarian Stage Calculator/Assets/Shield.png' width='50'/> Defence: <span id='PlayerDefenceReqOutput'>?</span><br><p>Your Stage Bonuses will also become:<br><br>Sunrise FM: <span id='StageBonusSunriseFMCurrentOutput'>?</span>x > <span id='StageBonusSunriseFMNextOutput'>?</span>x<br>Solar Rays: <span id='StageBonusSolarRaysCurrentOutput'>?</span>x > <span id='StageBonusSolarRaysNextOutput'>?</span>x<br>Sunstone: <span id='StageBonusSunstoneCurrentOutput'>?</span>x > <span id='StageBonusSunstoneNextOutput'>?</span>x<br>Solar Shards: <span id='StageBonusSolarShardsCurrentOutput'>?</span>x > <span id='StageBonusSolarShardsNextOutput'>?</span>x<br>Souls (hidden bonus): <span id='StageBonusSoulsCurrentOutput'>?</span>x > <span id='StageBonusSoulsNextOutput'>?</span>x</p></div></div>";
+	document.getElementById("SolarianStageCalculator").innerHTML = "<div id='SolarianStageCalculatorContainer' class='templatedesktop' style='color:#FFF;padding:1em;background:#2F0075;width:80%;margin:auto'><div class='templatedesktop' style='border-radius:initial;background:initial;width:10%;padding:0.5em;margin-top:0.5em'>Toggle Suffixes<button id='SSCSuffixToggleButton' style='background:#FF0000'>Disabled</button></div><div style='text-align:center;font-size:24px'><img src='https://acodingseal.github.io/Solarian Stage Calculator/Assets/Sword.png' width='100'/> Solarian Stage Calculator <img src='https://acodingseal.github.io/Solarian Stage Calculator/Assets/Shield.png' width='100'/><br><div style='font-size:initial'><p>Stage: <input id='CurrentStageInput' style='width:5%'/></p><p>Solarian Challenge: <input id='ChallengeInput' class='slider' type='range' value='0' min='0' max='2'/></p><p style='font-size:14px'>Selected challenge: <span id='SelectedChallengeText'/></p></div></div><table style='margin:auto;color:#FFF'><tr><td><b>Your offence:</b> <input id='PlayerOffenceInput' style='width:10%'/></td><td style='text-align:right'><b>Enemy offence:</b> <span id='EnemyOffenceOutput'/></td></tr><tr><td><b>Your defence:</b> <input id='PlayerDefenceInput' style='width:10%'/></td><td style='text-align:right'><b>Enemy defence:</b> <span id='EnemyDefenceOutput'/></td></tr><tr><td><b>Your maximum health (100x defence):</b> <span id='PlayerMaximumHealthOutput'/></td><td style='text-align:right'><b>Enemy maximum health (100x defence):</b> <span id='EnemyMaximumHealthOutput'/></td></tr><tr><td><b>Your current health (optional, default 100% of max health):</b> <input id='PlayerCurrentHealthInput' style='width:10%'/></td><td style='text-align:right'><b>Enemy current health (optional, default 100% of max health):</b> <input id='EnemyCurrentHealthInput' style='width:10%'/></td></tr></table><div style='margin-top:1em;text-align:center'><button id='SSCCalculateButton'>Calculate</button></div><br><div class='templatedesktop' style='background:initial;border-left:0;border-right:0;border-bottom:0;padding:0.5em;border-radius:initial;text-align:center'><div style='font-size:20px;font-weight:bold'>Result</div><br>At Stage <span id='CurrentStageOutput'>?</span>, the requirements to complete the stage, when currently <span id='CurrentHealthOutput'>?</span>% of maximum health, are:<br><br><img src='https://acodingseal.github.io/Solarian Stage Calculator/Assets/Sword.png' width='50'/> Offence: <span id='PlayerOffenceReqOutput'>?</span><br><img src='https://acodingseal.github.io/Solarian Stage Calculator/Assets/Shield.png' width='50'/> Defence: <span id='PlayerDefenceReqOutput'>?</span><br><p>Your Stage Bonuses will also become:<br><br>Sunrise FM: <span id='StageBonusSunriseFMCurrentOutput'>?</span>x > <span id='StageBonusSunriseFMNextOutput'>?</span>x<br>Solar Rays: <span id='StageBonusSolarRaysCurrentOutput'>?</span>x > <span id='StageBonusSolarRaysNextOutput'>?</span>x<br>Sunstone: <span id='StageBonusSunstoneCurrentOutput'>?</span>x > <span id='StageBonusSunstoneNextOutput'>?</span>x<br>Solar Shards: <span id='StageBonusSolarShardsCurrentOutput'>?</span>x > <span id='StageBonusSolarShardsNextOutput'>?</span>x<br>Souls (hidden bonus): <span id='StageBonusSoulsCurrentOutput'>?</span>x > <span id='StageBonusSoulsNextOutput'>?</span>x</p></div></div>";
 	
 	var playerOffence = new Decimal(1);
 	var playerDefence = new Decimal(0);
@@ -16,15 +16,31 @@ function addSolarianStageCalculator() {
 	var enemyStatScaling = new Decimal(10);
 	var enemyHealthScaling = new Decimal(10.3);
 	var result;
+	var suffixStatus = false;
+	const suffixes = ["", "", "M", "B", "T", "Qa", "Qt", "Sx", "Sp", "Oc", "No", "Dc", "Ud", "DDc", "Td", "Qad", "Qid", "Sxd", "Spd", "Ocd", "Nod", "Vg", "UVg", "DVg", "TVg", "QaVg", "QtVg", "SxVg", "SpVg", "OVg", "NVg", "Tg", "UTg", "DTg", "TTg", "QaTg", "QtTg", "SxTg", "SpTg", "OTg", "NTg", "Qd", "UQd", "DQd", "TQd", "QaQd", "QtQd", "SxQd", "SpQd", "OQd", "NQd", "Qi", "UQi", "DQi", "TQi", "QaQi", "QtQi", "SxQi", "SpQi", "OQi", "NQi", "He", "UHe", "DHe", "THe", "QaHe", "QtHe", "SxHe", "SpHe", "OHe", "NHe", "St", "USt", "DSt", "TSt", "QaSt", "QtSt", "SxSt", "SpSt", "OSt", "NSt", "Og", "UOg", "DOg", "TOg", "QaOg", "QtOg", "SxOg", "SpOg", "OOg", "NOg", "Nn", "UNn", "DNn", "TNn", "QaNn", "QtNn", "SxNn", "SpNn", "ONn", "NNn"];
+	const suffixesLC = ["", "", "m", "b", "t", "qa", "qt", "sx", "sp", "oc", "no", "dc", "ud", "ddc", "td", "qad", "qid", "sxd", "spd", "ocd", "nod", "vg", "uvg", "dvg", "tvg", "qavg", "qtvg", "sxvg", "spvg", "ovg", "nvg", "tg", "utg", "dtg", "ttg", "qatg", "qttg", "sxtg", "sptg", "otg", "ntg", "qd", "uqd", "dqd", "tqd", "qaqd", "qtqd", "sxqd", "spqd", "oqd", "nqd", "qi", "uqi", "dqi", "tqi", "qaqi", "qtqi", "sxqi", "spqi", "oqi", "nqi", "he", "uhe", "dhe", "the", "qahe", "qthe", "sxhe", "sphe", "ohe", "nhe", "st", "ust", "dst", "tst", "qast", "qtst", "sxst", "spst", "ost", "nst", "og", "uog", "dog", "tog", "qaog", "qtog", "sxog", "spog", "oog", "nog", "nn", "unn", "dnn", "tnn", "qann", "qtnn", "sxnn", "spnn", "onn", "nnn"];
 	function notateInt(e) {
+		var extraZeroes;
 		if (e.greaterThanOrEqualTo(1e3) && e.lessThan(1e6)) {
 			result = Number(e).toLocaleString();
+			} else if (e.greaterThanOrEqualTo(1e6) && e.lessThan(1e303) && suffixStatus === true) {
+				switch(((e.exponent / 3) - Math.floor(e.exponent / 3)).toFixed(1)) {
+					case "0.0":
+					extraZeroes = 0;
+					break;
+					case "0.3":
+					extraZeroes = 1;
+					break;
+					case "0.7":
+					extraZeroes = 2;
+					}
+					result = (e.mantissa * (10 ** extraZeroes)).toFixed(3) + "" + suffixes[Math.floor(e.exponent / 3)];
 			} else if (e.greaterThanOrEqualTo(1e6) && e.lessThan(1e21)) {
 				result = Number(e).toExponential(3).replace(/[+]/g, "");
 				} else {
-				result = e.toStringWithDecimalPlaces(3);
-			}
-			return result;
+					result = e.toStringWithDecimalPlaces(3);
+					}
+					return result;
 	}
 	function updateChallenge() {
 		switch(document.getElementById("ChallengeInput").value) {
@@ -153,6 +169,19 @@ function addSolarianStageCalculator() {
 	}
 	document.getElementById("ChallengeInput").addEventListener("input", updateChallenge);
 	document.getElementById("SSCCalculateButton").addEventListener("click", updateResults);
+	document.getElementById("SSCSuffixToggleButton").addEventListener("click", function() {
+		if (suffixStatus === false) {
+			document.getElementById("SSCSuffixToggleButton").setAttribute("style", "background:#00FF00");
+			document.getElementById("SSCSuffixToggleButton").innerHTML = "Enabled";
+			suffixStatus = true;
+			updateResults();
+		} else {
+			document.getElementById("SSCSuffixToggleButton").setAttribute("style", "background:#FF0000");
+			document.getElementById("SSCSuffixToggleButton").innerHTML = "Disabled";
+			suffixStatus = false;
+		}
+		updateResults();
+	});
 	updateResults();
 	} else {
 		console.log("[Solarian Stage Calculator] [LOG] Failed to locate ID or calculator already exists. Cancelling script.");
