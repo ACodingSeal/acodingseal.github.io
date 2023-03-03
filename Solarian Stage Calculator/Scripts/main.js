8c4812ac8f48d3f63554dcb1707092aa908531f0
ACodingSeal <119761723+ACodingSeal@users.noreply.github.com>
2023-03-03 19:46:45 +1100

    Update main.js

--- Solarian Stage Calculator/Scripts/main.js
+++ Solarian Stage Calculator/Scripts/main.js
@@ -367,7 +367,6 @@ function addSolarianStageCalculator() { // Function for ensuring all the calcula
 
 
             // Variables are currently declared outside their function for testing.
-            var maxDivide;
             var playerMaxHits = (playerCurrentHealth.add(playerDefence)).dividedBy(enemyOffence);
             var enemyMaxHits = (enemyCurrentHealth.add(enemyDefence)).dividedBy(playerOffence);
             var playerOffenceReq = playerOffence;
