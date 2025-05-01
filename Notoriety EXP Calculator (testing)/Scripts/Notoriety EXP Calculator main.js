17a7676ad0e453c3f0f788af6f2d00bff6c967f2
ACodingSeal <119761723+ACodingSeal@users.noreply.github.com>
2025-05-01 15:00:27 +1000

    Update Notoriety EXP Calculator main.js

--- Notoriety EXP Calculator (testing)/Scripts/Notoriety EXP Calculator main.js
+++ Notoriety EXP Calculator (testing)/Scripts/Notoriety EXP Calculator main.js
@@ -362,7 +362,7 @@
 	function addHallofInfamyCCLs() {
 		elem.SectionContainer_HallofInfamyCCLs.innerHTML = '';
 		const grassAvoiders = 31;
-		var string = "<div style='text-align:center'>At the end of the day, when we're no longer around, we become stories, a relic of the past, and we generally want to be remembered. To live on in society's collective minds, to ensure our story is told and retold countless times. That is true immortality. For their impressive achievement of Infamy Rank 250 (CCL) in Notoriety, the following people have earned a permanent recognition in the Hall of CCLs.<p></p>Badge obtainment times are noted in the local system time in <b>year-month-day 24hour:minute:second:millisecond</b> format. 'Time elapsed since badge obtainment' is actual elapsed time, and is updated on tool load or changing the 'Time output format' toggle setting. Entries' user-written descriptions' timestamps are usually plaintext, so they do not auto-update to the system time. Entries may take some time to be added, especially depending on available data. There are currently many missing user-written descriptions. Roblox avatars must be updated manually with a tool update. Open images in a new tab to view the full image. If you wish for modifications to be made to your entry, or want it to be anonymised, contact the tool creator on Discord (same username).</div>";
+		var string = "<div style='text-align:center'>At the end of the day, when we're no longer around, we become stories, a relic of the past, and we generally want to be remembered. To live on in society's collective minds, to ensure our story is told and retold countless times. That is true immortality. The following people have achieved Infamy Rank 250 (CCL) in Notoriety. For their intense commitment to this impressive achievement, they have earned a permanent recognition in the Hall of CCLs.<p></p>Badge obtainment times are noted in the local system time in <b>year-month-day 24hour:minute:second:millisecond</b> format. 'Time elapsed since badge obtainment' is actual elapsed time, and is updated on tool load or changing the 'Time output format' toggle setting. Entries' user-written descriptions' timestamps are usually plaintext, so they do not auto-update to the system time. Entries may take some time to be added, especially depending on available data. There are currently many missing user-written descriptions. Roblox avatars must be updated manually with a tool update. Open images in a new tab to view the full image. If you wish for modifications to be made to your entry, or want it to be anonymised, contact the tool creator on Discord (same username).</div>";
 		function Player(username, displayName, robloxAvatar, whenCCL, timeSincePrevious, position, notes, ownDescription) {
 			this.username = username;
 			this.displayName = displayName;
@@ -982,7 +982,7 @@
 			${updateLogEntry('other', 'Other')}
 		Major tool versions are <u>underlined</u>. All timestamps in the Update Log are noted in UTC.
 		<p/>
-		Estimated total active development time across all versions: ~55 hours, 41 minutes.
+		Estimated total active development time across all versions: ~55 hours, 43 minutes.
 		<p/>
 		Report any issues or suggestions about this tool to the tool creator, or <a href='https://github.com/ACodingSeal/acodingseal.github.io/issues'>open an issue</a>.
 		<p/>
@@ -994,7 +994,7 @@
 		<b>[Testing] Version 0.1.1d</b>
 		<ul>
 			${updateLogEntry('add', "Menu Miscellaneous > Section Hall of CCLs: Prepended an expansion to the top-of-section notes.")}
-			${updateLogEntry('other', "Estimated active development time: ~6 minutes.")}
+			${updateLogEntry('other', "Estimated active development time: ~8 minutes.")}
 		</ul></p>
 		<p>
 		<b>[2025-05-01 04:43] Version 0.1.1c</b>
