850ff831fc2bd4a35f7034bf85cab5efc8b1c61b
ACodingSeal <119761723+ACodingSeal@users.noreply.github.com>
2025-05-14 21:37:51 +1000

    Update Notoriety EXP Calculator main.js

--- Notoriety EXP Calculator (testing)/Scripts/Notoriety EXP Calculator main.js
+++ Notoriety EXP Calculator (testing)/Scripts/Notoriety EXP Calculator main.js
@@ -1603,7 +1603,7 @@
 			${updateLogEntry('other', 'Other')}
 		Prominent tool versions are <u>underlined</u>. Update Log version timestamps are noted in the local system time in <b>year-month day 24hour:minute</b> format.
 		<p></p>
-		Estimated total active development time across all versions: ~76 hours, 32 minutes.
+		Estimated total active development time across all versions: ~76 hours, 17 minutes.
 		<p></p>
 		Report any issues or suggestions about this tool to the tool creator, or <a href='https://github.com/ACodingSeal/acodingseal.github.io/issues'>open an issue</a>.
 		<p></p>
@@ -1633,7 +1633,7 @@
 			${updateLogEntry('fix', "Menu Calculator > Section Money Settings: Fixed a bug where the 'Desired money' input would be visible with 'Computing' toggle setting set to 'EXP, Levels & Infamy'.")}
 			${updateLogEntry('fix', "Fixed a bug where the tool's formatted timestamps would not correctly display milliseconds.")}
 			${updateLogEntry('other', "Tool versions are now based on: Major.MediumtoLarge.Small(letter representing a quick patch), with larger versions 'resetting' previous versions back to 0 and no letter. Starting in this version (updated from x.2.1 > x.3.0), all Update Log versions are now 1.x.x higher, as the first released version should've been 1.0.0 instead of 0.0.1.")}
-			${updateLogEntry('other', "Estimated active development time: ~11 hours, 32 minutes.")}
+			${updateLogEntry('other', "Estimated active development time: ~11 hours, 37 minutes.")}
 		</ul></div>
 		<div class='NotorietyEXPCalculator_UpdateLogVersionEntry'>
 		<b>${versionDateStrings['0.2.1']} Version 0.2.1</b>
